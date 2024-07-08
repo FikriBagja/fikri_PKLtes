fbc4c6c33c3dd62f5c378caa3088692da87a50a1
FikriBagja <fikribr10@gmail.com>
2024-07-08 20:40:48 +0700

    nambah input

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-const bentukArray = [
-  { backgroundColor: "gray", borderRadius: "0" },
-  { borderRadius: "50%" },
-  { borderRadius: "0" },
-  { borderRadius: "50%" },
-];
-
-let i = 0;
-
-function ubahbentuk() {
-  const warna1 = document.getElementById("warna1").value;
-  const warna2 = document.getElementById("warna2").value;
-  const warna3 = document.getElementById("warna3").value;
-
-  bentukArray[1].backgroundColor = warna1;
-  bentukArray[2].backgroundColor = warna2;
-  bentukArray[3].backgroundColor = warna3;
-
-  i = (i + 1) % bentukArray.length;
-  const elemenbentuk = document.getElementById("bentuk");
-  const bentukBerikutnya = bentukArray[i];
-  elemenbentuk.style.backgroundColor = bentukBerikutnya.backgroundColor;
-  elemenbentuk.style.borderRadius = bentukBerikutnya.borderRadius;
+let sub = document.getElementById("sub")
+sub.style.border = "none"
+sub.style.padding = "10px"
+function masuk() {
+    sub.style.background = "blue"
+    sub.style.color = "white"
 }
+function keluar() {
+    sub.style.background = ""
+    sub.style.color = "black"
+}
+
+const bentukArray = [
+    { backgroundColor: "gray", borderRadius: "0" },
+    { borderRadius: "50%" },
+    { borderRadius: "0" },
+    { borderRadius: "50%" },
+  ];
+  
+  let i = 0;
+  
+  function ubahbentuk() {
+    const warna1 = document.getElementById("warna1").value;
+    const warna2 = document.getElementById("warna2").value;
+    const warna3 = document.getElementById("warna3").value;
+  
+    bentukArray[1].backgroundColor = warna1;
+    bentukArray[2].backgroundColor = warna2;
+    bentukArray[3].backgroundColor = warna3;
+  
+    i = (i + 1) % bentukArray.length;
+    const elemenbentuk = document.getElementById("bentuk");
+    const bentukBerikutnya = bentukArray[i];
+    elemenbentuk.style.backgroundColor = bentukBerikutnya.backgroundColor;
+    elemenbentuk.style.borderRadius = bentukBerikutnya.borderRadius;
+  }
+  

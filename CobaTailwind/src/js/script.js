58198fefd11b6d9a2e43d7cf7b82a8da7e71b1a0
FikriBagja <fikribr10@gmail.com>
2024-07-14 15:42:41 +0700

    update

--- CobaTailwind/src/js/script.js
+++ CobaTailwind/src/js/script.js
@@ -1,9 +1,54 @@
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const ToTop = document.querySelector("#to-top");
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    ToTop.classList.remove("hidden");
+    ToTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    ToTop.classList.remove("flex");
+    ToTop.classList.add("hidden");
+  }
+};
+
 //hamburger
 
-const hamburger = document.querySelector('#hamburger');
-const navMenu =document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
+
+window.addEventListener("click", function (e) {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
